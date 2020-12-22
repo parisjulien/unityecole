1b5ef389b865925ab977aaae0505c838a5d2e3b4
Julien Paris <julien.paris@mycarspot.io>
2020-12-22 09:22:45 +0100

    Enemy can move

---
+++ Assets/_GAME/Enemy/EnemyPatrol.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Move", 2F);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void Move()
+    {
+        agent.SetDestination(new Vector3(transform.position.x, transform.position.y, transform.position.z + 4));
+    }
+}

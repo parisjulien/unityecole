fb48c388350cf9c0613e4ad98d6bd325122f71c4
Azuri902 <46462194+Azuri902@users.noreply.github.com>
2020-12-26 12:50:02 +0100

    Barre de vie
    Manque la gestion des hits avec les ennemis

---
+++ Assets/_GAME/_FINAL/Barres/BarreDeVie.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BarreDeVie : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+
+    public void SetMaxPV(int pv)
+    {
+        slider.maxValue = pv;
+        slider.value = pv;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetPv(int pv)
+    {
+        slider.value = pv;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
---
+++ Assets/_GAME/_FINAL/Characters/Player/JoueurVie.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class JoueurVie : MonoBehaviour
+{
+
+    public int maxPV = 100;
+    public int PVCourant;
+
+    public BarreDeVie BarreDeVie;
+
+    void Start()
+    {
+        PVCourant = maxPV;
+        BarreDeVie.SetMaxPV(maxPV);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Dommages(int nDegats)
+    {
+        PVCourant -= nDegats;
+        BarreDeVie.SetPv(PVCourant);
+    }
+
+}

d1c39d053322438f7dd0173b97492f6800faf84b
Julien Paris <julien.paris@mycarspot.io>
2020-12-28 21:39:47 +0100

    Modification de la camera pas fini

---
+++ Assets/_GAME/_FINAL/Camera/CameraFolow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFolow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed;
+    public float speed = 3.5f;
+    private float X;
+    private float Y;
+
+    public void FixedUpdate()
+    {
+        transform.position = new Vector3(target.position.x, 5, target.position.z - 1.5F);
+    }
+
+}

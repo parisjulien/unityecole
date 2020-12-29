4a549ae1c1c5766c2b4f18b10f81f0fb816248f4
Julien Paris <julien.paris@mycarspot.io>
2020-12-29 11:22:23 +0100

    camera ok

--- Assets/_GAME/_FINAL/Camera/CameraFolow.cs
+++ Assets/_GAME/_FINAL/Camera/CameraFolow.cs
@@ -6,14 +6,9 @@ public class CameraFolow : MonoBehaviour
 {
     public Transform target;
 
-    public float smoothSpeed;
-    public float speed = 3.5f;
-    private float X;
-    private float Y;
-
     public void FixedUpdate()
     {
-        transform.position = new Vector3(target.position.x, 5, target.position.z - 1.5F);
+        transform.position = new Vector3(target.position.x, 7, target.position.z - 1.5F);
     }
 
 }
